a0859504af7860692b525c108df80c5e2699a645
Joostlek <joostlek@outlook.com>
2024-06-14 13:13:17 +0200

    Improve all

--- src/aiomealie/__init__.py
+++ src/aiomealie/__init__.py
@@ -1,12 +1,26 @@
 """Asynchronous Python client for Mealie."""
 
-from aiomealie.exceptions import MealieConnectionError
+from aiomealie.exceptions import MealieConnectionError, MealieError
 from aiomealie.mealie import MealieClient
-from aiomealie.models import StartupInfo, Theme
+from aiomealie.models import (
+    StartupInfo,
+    Theme,
+    Recipe,
+    RecipesResponse,
+    Mealplan,
+    MealplanResponse,
+    MealplanEntryType,
+)
 
 __all__ = [
     "MealieConnectionError",
+    "MealieError",
     "MealieClient",
     "StartupInfo",
     "Theme",
+    "Recipe",
+    "RecipesResponse",
+    "Mealplan",
+    "MealplanResponse",
+    "MealplanEntryType",
 ]
